7f25d41d0c58af5c7bfa3ac20cfb129c6be63231
oadiliosantos <oadiliosantos@gmail.com>
2022-01-19 16:01:22 -0300

    global erros e hash com md5

--- backend/src/resources/user/user.service.ts
+++ backend/src/resources/user/user.service.ts
@@ -1,6 +1,6 @@
 import { getRepository } from "typeorm"
 import { User } from "../../entity/User"
-// import md5 from 'crypto-js/md5'
+import md5 from 'crypto-js/md5'
 
 import {UserSignIn} from "./dtos/user.signin.dtos"
 import {UserSignUp} from "./dtos/user.signup.dtos"
@@ -10,8 +10,9 @@ export default class UserService {
     async signIn(user: UserSignIn) {
         const userRepository = getRepository(User)
         const {email, password} = user
+        const passwordHash = md5(password).toString()
 
-        const existuser = await userRepository.findOne({where: {email, password}})
+        const existuser = await userRepository.findOne({where: {email, password: passwordHash}})
 
         if(!existuser) {
             return null
